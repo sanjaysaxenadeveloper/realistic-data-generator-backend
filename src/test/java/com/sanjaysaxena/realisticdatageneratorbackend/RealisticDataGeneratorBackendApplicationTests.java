0c26fc43dd990709b0c83ae5e11b9bca8eeb4b54
Sanjay Saxena <sanjaysaxenadeveloper@gmail.com>
2024-05-02 16:26:48 +0530

    Initial commit

---
+++ src/test/java/com/sanjaysaxena/realisticdatageneratorbackend/RealisticDataGeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjaysaxena.realisticdatageneratorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealisticDataGeneratorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
